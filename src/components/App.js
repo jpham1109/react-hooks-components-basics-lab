11e43b080f508cbde1069a5c25253b9732b4eeeb
Jamie H Pham <jpham1109@gmail.com>
2021-04-05 15:38:58 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -16,12 +16,20 @@ function Home() {
   );
 }
 
+function About () {
+  return (
+    <div id="about">
+      <p>Learning</p>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* write an <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
